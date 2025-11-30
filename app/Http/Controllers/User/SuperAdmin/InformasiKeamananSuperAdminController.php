c03946875ed32d75066725f5b4fdd8813f4aab50
Imamul Mutaqin Al Hanif <imamulmutaqinalhanif@gmail.com>
2025-11-30 10:10:50 +0700

    Penyiapan controller Super Admin

---
+++ app/Http/Controllers/User/SuperAdmin/InformasiKeamananSuperAdminController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\User\SuperAdmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class InformasiKeamananSuperAdminController extends Controller
+{
+    //
+}
